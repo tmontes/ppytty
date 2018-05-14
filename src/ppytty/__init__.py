669cf07ec5c848c19f01d22e573af925f4e3a552
Tiago Montes <tiago.montes@gmail.com>
2018-05-14 10:09:32 +0100

    Minimal packaging + License in place.

---
+++ src/ppytty/__init__.py
@@ -0,0 +1,35 @@
+# ----------------------------------------------------------------------------
+# ppytty
+# ----------------------------------------------------------------------------
+# Copyright (c) Tiago Montes.
+# See LICENSE for deatils.
+# ----------------------------------------------------------------------------
+
+"""
+ppytty
+"""
+
+from __future__ import absolute_import
+
+
+
+__version__ = '1.0.0a0'
+
+__title__ = 'ppytty'
+__description__ = 'ppytty'
+
+__license__ = 'MIT'
+__uri__ = 'https://github.com/tmontes/ppytty/'
+
+__author__ = 'Tiago Montes'
+__email__ = 'tiago.montes@gmail.com'
+
+
+from . _script import Script
+
+__all__ = [
+    'Script',
+]
+
+
+# ----------------------------------------------------------------------------
