47fbaa6e50c75c5e5ccb3bf7d74cc20b8452ca7e
Tiago Montes <tiago.montes@gmail.com>
2018-05-20 17:24:25 +0100

    Adding names to tasks in nonstop_level1 example for easier diagnostic.

--- examples/nonstop_level1.py
+++ examples/nonstop_level1.py
@@ -6,15 +6,15 @@ km = {
 
 ppytty_task = ppytty.OuterSequenceKeyboard([
     ppytty.Serial([
-        ppytty.Label('first label'),
-        ppytty.Loop(ppytty.DelayReturn(seconds=3)),
-    ]),
+        ppytty.Label('first label', name='l1'),
+        ppytty.Loop(ppytty.DelayReturn(seconds=30, name='d1'), name='l1'),
+    ], name='s1'),
     ppytty.Serial([
-        ppytty.Label('second label'),
-        ppytty.Loop(ppytty.DelayReturn(seconds=3)),
-    ]),
+        ppytty.Label('second label', name='l2'),
+        ppytty.Loop(ppytty.DelayReturn(seconds=30, name='d2'), name='l2'),
+    ], name='s2'),
     ppytty.Serial([
-        ppytty.Label('third label'),
-        ppytty.Loop(ppytty.DelayReturn(seconds=3)),
-    ]),
+        ppytty.Label('third label', name='l3'),
+        ppytty.Loop(ppytty.DelayReturn(seconds=30, name='d3'), name='l3'),
+    ], name='s3'),
 ], name='outer')
