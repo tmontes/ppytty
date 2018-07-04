7ae3bffd08d4aed645c1d9fcf4fdcad7749ee6ee
Tiago Montes <tiago.montes@gmail.com>
2018-07-04 16:17:49 +0100

    Fix task-destroy trap: now handles tasks waiting for messages.

--- src/ppytty/kernel/trap_handlers.py
+++ src/ppytty/kernel/trap_handlers.py
@@ -184,6 +184,8 @@ def task_destroy(task, user_child_task, keep_running=True):
     elif child_task in state.tasks_waiting_time:
         state.tasks_waiting_time.remove(child_task)
         common.clear_tasks_waiting_time_hq()
+    elif child_task in state.tasks_waiting_inbox:
+        state.tasks_waiting_inbox.remove(child_task)
     elif child_task in state.completed_tasks:
         del state.completed_tasks[child_task]
     else:
