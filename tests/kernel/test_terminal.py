206351a539606d22744b4ded4a88f28bdf5331de
Tiago Montes <tiago.montes@gmail.com>
2018-07-05 14:00:00 +0100

    First terminal feed test.

--- tests/kernel/test_terminal.py
+++ tests/kernel/test_terminal.py
@@ -52,4 +52,32 @@ class Test(io_bypass.NoOutputTestCase):
         self.assertEqual(payload_bytes, 80 * 25 * b' ')
 
 
+    def test_feed_text_then_render(self):
+
+        self._discard_first_render()
+
+        plain_text = b'this is the terminal'
+        self.t.feed(plain_text)
+        self.t.render()
+        written_bytes = self.get_os_written_bytes()
+
+        # The byte sequence used to position the cursor.
+        tigetstr_cup = self.fake_tigetstr('cup')
+
+        # Should start by positioning the cursor at (col=0, row=0).
+        prefix = self.fake_tparm(tigetstr_cup, 0, 0)
+        self.assertTrue(written_bytes.startswith(prefix), 'wrong prefix')
+
+        # Should end by positioning the cursor at (col=len(plain_text), row=0).
+        suffix = self.fake_tparm(tigetstr_cup, 0, len(plain_text))
+        self.assertTrue(written_bytes.endswith(suffix), 'wrong suffix')
+
+        # Payload should be a full line (plain_text is shorter than that): the
+        # test terminal is 80 column wide, so the payload should start with the
+        # plan_text and be filled with spaces.
+        payload = self.strip_fake_curses_entries(written_bytes)
+        expected = plain_text + b' ' * (80 - len(plain_text))
+        self.assertEqual(payload, expected)
+
+
 # ----------------------------------------------------------------------------
