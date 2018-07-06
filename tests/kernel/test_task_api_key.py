455d664c8f4171e2469ed6efece3128a569a5471
Tiago Montes <tiago.montes@gmail.com>
2018-07-06 18:29:15 +0100

    Updated kernel keyboard read tests to new trap API.

--- tests/kernel/test_task_api_key.py
+++ tests/kernel/test_task_api_key.py
@@ -5,7 +5,7 @@
 # See LICENSE for details.
 # ----------------------------------------------------------------------------
 
-from ppytty.kernel import run, scheduler
+from ppytty.kernel import run, api, scheduler
 
 from . import helper_io
 from . import helper_log
@@ -16,8 +16,8 @@ class Test(helper_io.NoOutputAutoTimeControlledInputTestCase):
 
     def test_read_key(self):
 
-        def task():
-            result = yield ('read-key', 100)
+        async def task():
+            result = await api.key_read(priority=100)
             return result
 
         self.input_control.feed_data(b'x')
@@ -30,15 +30,15 @@ class Test(helper_io.NoOutputAutoTimeControlledInputTestCase):
 
     def test_blocking_read_key(self):
 
-        def child():
-            byte_key = yield ('read-key', 100)
+        async def child():
+            byte_key = await api.key_read(priority=100)
             return byte_key
 
-        def parent(child_task):
-            yield ('task-spawn', child_task)
-            yield ('sleep', 1)
+        async def parent(child_task):
+            await api.task_spawn(child_task)
+            await api.sleep(1)
             self.input_control.feed_data(b'k')
-            task_wait_result = yield ('task-wait',)
+            task_wait_result = await api.task_wait()
             return task_wait_result
 
         child_task = child()
@@ -54,21 +54,21 @@ class Test(helper_io.NoOutputAutoTimeControlledInputTestCase):
 
     def test_read_key_priority(self):
 
-        def child(read_key_priority):
-            byte_key = yield ('read-key', read_key_priority)
+        async def child(read_key_priority):
+            byte_key = await api.key_read(priority=read_key_priority)
             return byte_key
 
-        def parent(hi_pri_reader, lo_pri_reader):
+        async def parent(hi_pri_reader, lo_pri_reader):
             all_tasks = {hi_pri_reader, lo_pri_reader}
-            yield ('task-spawn', lo_pri_reader)
-            yield ('task-spawn', hi_pri_reader)
-            yield ('sleep', 1)
+            await api.task_spawn(lo_pri_reader)
+            await api.task_spawn(hi_pri_reader)
+            await api.sleep(1)
             self.input_control.feed_data(b'i')
-            completed_task, child_success, child_result = yield ('task-wait',)
+            completed_task, child_success, child_result = await api.task_wait()
             all_tasks.remove(completed_task)
             other_task = all_tasks.pop()
-            yield ('task-destroy', other_task)
-            destroyed_task, _, _ = yield ('task-wait',)
+            await api.task_destroy(other_task)
+            destroyed_task, _, _ = await api.task_wait()
             return completed_task, child_success, child_result, destroyed_task
 
         hi_pri_reader = child(100)
@@ -87,26 +87,26 @@ class Test(helper_io.NoOutputAutoTimeControlledInputTestCase):
 
     def test_put_key(self):
 
-        def hi_pri_child():
-            byte_key = yield ('read-key', 100)
-            yield ('put-key', byte_key)
-            yield ('sleep', 42)
+        async def hi_pri_child():
+            byte_key = await api.key_read(priority=100)
+            await api.key_unread(byte_key)
+            await api.sleep(42)
 
-        def lo_pri_child():
-            byte_key = yield ('read-key', 1000)
+        async def lo_pri_child():
+            byte_key = await api.key_read(priority=1000)
             return byte_key
 
-        def parent(hi_pri_reader, lo_pri_reader):
+        async def parent(hi_pri_reader, lo_pri_reader):
             all_tasks = {hi_pri_reader, lo_pri_reader}
-            yield ('task-spawn', lo_pri_reader)
-            yield ('task-spawn', hi_pri_reader)
-            yield ('sleep', 1)
+            await api.task_spawn(lo_pri_reader)
+            await api.task_spawn(hi_pri_reader)
+            await api.sleep(1)
             self.input_control.feed_data(b't')
-            completed_task, child_success, child_result = yield ('task-wait',)
+            completed_task, child_success, child_result = await api.task_wait()
             all_tasks.remove(completed_task)
             other_task = all_tasks.pop()
-            yield ('task-destroy', other_task)
-            destroyed_task, _, _ = yield ('task-wait',)
+            await api.task_destroy(other_task)
+            destroyed_task, _, _ = await api.task_wait()
             return completed_task, child_success, child_result, destroyed_task
 
         success, result = run(parent(hi_pri_child, lo_pri_child))
@@ -121,9 +121,9 @@ class Test(helper_io.NoOutputAutoTimeControlledInputTestCase):
 
     def test_kernel_run_stops_with_double_q_input(self):
 
-        def task():
+        async def task():
             while True:
-                yield ('sleep', 42)
+                await api.sleep(42)
 
         self.input_control.feed_data(b'qq')
 
@@ -135,8 +135,8 @@ class Test(helper_io.NoOutputAutoTimeControlledInputTestCase):
 
     def test_kernel_run_does_not_stop_with_q_and_something_else(self):
 
-        def task():
-            yield ('sleep', 42)
+        async def task():
+            await api.sleep(42)
             return 'confirming-regular-completion'
 
         self.input_control.feed_data(b'qx')
@@ -150,8 +150,8 @@ class Test(helper_io.NoOutputAutoTimeControlledInputTestCase):
 
     def test_kernel_run_dumps_state_with_capital_d_input(self):
 
-        def task():
-            yield ('sleep', 1)
+        async def task():
+            await api.sleep(1)
 
         log_handler = helper_log.create_and_add_handler()
         self.addCleanup(lambda: helper_log.remove_handler(log_handler))
