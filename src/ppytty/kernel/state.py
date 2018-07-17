87468f8b9a2698e06521138939e852db7888e307
Tiago Montes <tiago.montes@gmail.com>
2018-07-17 18:54:23 +0100

    Kernel state no longer needs user_out_fd.

--- src/ppytty/kernel/state.py
+++ src/ppytty/kernel/state.py
@@ -99,7 +99,6 @@ class _State(object):
 
         self.in_fds = {}
         self.out_fds = []
-        self.user_out_fd = None
 
         # ---------------------------------------------------------------------
         # Environment.
@@ -125,7 +124,6 @@ class _State(object):
 
         self.terminal = terminal
         self.in_fds[terminal.in_fd] = None
-        self.user_out_fd = terminal.out_fd
         self.out_fds.append(terminal.out_fd)
 
 
