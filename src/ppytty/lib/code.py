57f8c49bd1a7608b9a3cdd41cba1716ce80a6853
Tiago Montes <tiago.montes@gmail.com>
2018-08-13 14:18:19 +0100

    Make paraiso-dark the default pygments style for Code widgets.

--- src/ppytty/lib/code.py
+++ src/ppytty/lib/code.py
@@ -31,8 +31,9 @@ from . import widget
 class Code(widget.WindowWidget):
 
     def __init__(self, code=None, file_name=None, file_encoding='utf-8',
-                 pygm_lexer_name='python3', pygm_style_name='default', id=None,
-                 template_slot=None, geometry=None, color=None, padding=None):
+                 pygm_lexer_name='python3', pygm_style_name='paraiso-dark',
+                 id=None, template_slot=None, geometry=None, color=None,
+                 padding=None):
 
         super().__init__(id=id, template_slot=template_slot, geometry=geometry,
                          color=color, padding=padding)
